be2a5cd0a45020b2519079e1fd5ef3e31b1ff545
chj <chenghongjia@buzzbreaknews.com>
2020-12-01 21:13:37 +0800

    update

--- utils/constants.py
+++ utils/constants.py
@@ -5,6 +5,6 @@ ONE_MINUTE = 60
 ONE_HOUR = 3600
 ONE_DAY = 86400
 
-COUNTRY_CODE = ['ID', 'PH', 'TH', 'AR']
+COUNTRY_CODE = ['ID', 'PH', 'TH', 'AR', 'BR']
 PLACEMENT = ['home_tab_for_you', 'news_detail_activity']
 INDICATOR_DIMENSION = ['control', 'treatment']
