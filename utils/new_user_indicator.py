b6d34d2c10363c05b05c272d56fbe09c85bed8f3
chj <chenghongjia@buzzbreaknews.com>
2020-12-03 19:49:10 +0800

    update

--- utils/new_user_indicator.py
+++ utils/new_user_indicator.py
@@ -84,7 +84,6 @@ class NewUserIndicator(object):
             flag = True
         return inser_sql, flag
 
-
     # 组装查询sql，并将统计计算结果存入mysql
     def compute_data(self):
         flag = False
@@ -103,23 +102,6 @@ class NewUserIndicator(object):
         # big_query_sql = self.get_big_query_sql_user_profile("buzzbreak-model-240306.input.accounts", "phone_No")
         # 结果数据存入数据库
         cursor = mysql_client.cursor()
-        inser_sql = "INSERT INTO " + self.table_name + " (treatment_name, placement, country_code, dimension, ctr, ctr_union, start_time, end_time, create_time) VALUES"
-        now_time_utc = datetime.datetime.utcnow()
-        flag = False
-        for key in impression_data_union.keys():
-            click_num = click_data.get(key, 0)
-            impression_num = impression_data.get(key, 0)
-            if impression_num <= 0:
-                continue
-            impression_num_union = impression_data_union.get(key)
-            if impression_num_union <= 0:
-                continue
-            temp_data = key.split("&&")
-            if len(temp_data) < 4:
-                continue
-            inser_sql = inser_sql + " ('" + temp_data[1] + "','" + temp_data[0] + "','" + temp_data[2] + "','" + temp_data[3] + "'," + str(round(click_num/impression_num, 5)) + "," + str(round(click_num/impression_num_union, 5)) + ",'" + self.start_time.strftime("%Y-%m-%d %H:%M:%S") + "','" + self.end_time.strftime("%Y-%m-%d %H:%M:%S") + "','" + now_time_utc.strftime("%Y-%m-%d %H:%M:%S") + "'),"
-            flag = True
-
         if flag:
             inser_sql = inser_sql[:len(inser_sql)-1]
             try:
