a112f0ae69059a2e54b9040018746ad3a4077d66
Hang <xionghang100@163.com>
2020-12-26 18:54:12 +0800

    test

--- main/main_day.py
+++ main/main_day.py
@@ -14,6 +14,7 @@ from utils import ctr_people
 from utils import news_ctr_notification_new_user
 from utils import news_ctr_notification_old_user
 from utils import video_ctr_notification_new_user
+from utils import video_ctr_notification_old_user
 from utils import video_ctr
 from utils import video_ctr_people
 from utils import new_users_retention_news_event
@@ -38,6 +39,7 @@ KIND = {
     "news_ctr_notification_new_user": 1,  # 新用户新闻推送点击率
     "news_ctr_notification_old_user": 1,  # 老用户新闻推送点击率
     "video_ctr_notification_new_user": 1,   # 新用户视频推送点击率
+    "video_ctr_notification_old_user": 1,   # 新用户视频推送点击率
     "new_users_retention_news_event": 1,   # 新闻用户留存率
     "video_ctr": 1,   # 视频ctr
     "video_ctr_people": 1,   # 视频 click_user_ratio
@@ -176,6 +178,8 @@ if __name__ == "__main__":
         news_ctr_notification_old_user.NewsCtrNotificationOldUserData(start_time, end_time, country_code, indicator_dimension, "day_news_ctr_notification_old_user").compute_data()
     elif kind == "video_ctr_notification_new_user":
         video_ctr_notification_new_user.VideoCtrNotificationNewUserData(start_time, end_time, country_code, indicator_dimension, "day_video_ctr_notification_new_user").compute_data()
+    elif kind == "video_ctr_notification_old_user":
+        video_ctr_notification_old_user.VideoCtrNotificationOldUserData(start_time, end_time, country_code, indicator_dimension, "day_video_ctr_notification_old_user").compute_data()
     elif kind == "new_users_retention_news_event":
         new_users_retention_news_event.NewUsersRetentionNewsEvent(start_time, end_time, country_code, indicator_dimension, "new_users_retention_news_event").compute_data()
     elif kind == "video_ctr":
