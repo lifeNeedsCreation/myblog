d6df22fc0a3bb9a24833ee08c71a235cba768d3c
Teddy Xiong <teddyxiong@buzzbreaknews.com>
2020-12-29 14:34:40 +0800

    test4

--- utils/news_click_ctr_new_user.py
+++ utils/news_click_ctr_new_user.py
@@ -69,37 +69,37 @@ class NewsClickCtrNewUserData(object):
         # 新用户新闻曝光数据
         impression_sql = \
             f"""
-            select result.country_code as country_code, result.placement as placement, result.key as key, result.value as value, count(result.news_id) as num from
-            (select account_news_impression.account_id as account_id, account_news_impression.news_id as news_id, account_news_impression.country_code as country_code, account_news_impression.placement as placement, memories.key as key, memories.value as value from 
-            (select account_id, country_code, news_impression.created_at, news_impression.placement, news_impression.news_id from
-            (select account_id, created_at, json_extract(data, '$.placement') as placement, json_extract(data, '$.id') as news_id from `stream_events.news_impression` as news_impression where news_impression.created_at > '{start_time}' and news_impression.created_at < '{end_time}' and json_value(data, '$.placement') in ("home_tab_for_you", 'news_detail_activity')) as news_impression
+            select result.country_code as country_code, result.placement as placement, result.key as key, result.value as value, count(result.account_id) as num from
+            (select account_news_impression.account_id as account_id, account_news_impression.country_code as country_code, account_news_impression.placement as placement, memories.key as key, memories.value as value from 
+            (select account_id, country_code, news_impression.created_at, news_impression.placement from
+            (select account_id, created_at, json_extract(data, '$.placement') as placement from `stream_events.news_impression` as news_impression where news_impression.created_at > '{start_time}' and news_impression.created_at < '{end_time}' and json_value(data, '$.placement') in ("home_tab_for_you", 'news_detail_activity')) as news_impression
             left join input.accounts as accounts on accounts.id = news_impression.account_id where accounts.created_at > '{start_time}' and accounts.created_at < '{end_time}' and accounts.name is not null and accounts.country_code in ({self.country_code})) as account_news_impression
             left join 
-            (select account_id, key, value, updated_at from partiko.memories where key like 'experiment%' and value in ({self.indicator_dimension})) as memories on memories.account_id = account_news_impression.account_id where key is not null and memories.updated_at <= account_news_impression.created_at) as result
-            group by news_id, country_code, placement, key, value
+            (select account_id, key, value, updated_at from partiko.memories where key like 'experiment%' and value in {self.indicator_dimension}) as memories on memories.account_id = account_news_impression.account_id where key is not null and memories.updated_at <= account_news_impression.created_at) as result
+            group by country_code, placement, key, value
             """
         impression_data = self.get_data(impression_sql)
 
         # 非曝光情况下用户点击数据
         impression_union_sql = \
             f"""
-            select country_code as country_code, placement as placement, key as key, value as value, count(news_id) as num from
-            (select account_news_click.account_id as account_id, account_news_click.news_id as news_id, account_news_click.country_code as country_code, account_news_click.placement as placement, memories.key as key, memories.value as value from 
-            (select account_id, country_code, news_click.created_at, news_click.placement, news_click.news_id from
-            (select account_id, created_at, json_extract(data, '$.placement') as placement, json_extract(data, '$.news_post.id') as news_id from `stream_events.news_click` as news_click where news_click.created_at > '{start_time}' and news_click.created_at < '{end_time}' and json_value(data, '$.placement') in ("home_tab_for_you", 'news_detail_activity')) as news_click
+            select country_code as country_code, placement as placement, key as key, value as value, count(account_id) as num from
+            (select account_news_click.account_id as account_id, account_news_click.country_code as country_code, account_news_click.placement as placement, memories.key as key, memories.value as value from 
+            (select account_id, country_code, news_click.created_at, news_click.placement from
+            (select account_id, created_at, json_extract(data, '$.placement') as placement from `stream_events.news_click` as news_click where news_click.created_at > '{start_time}' and news_click.created_at < '{end_time}' and json_value(data, '$.placement') in ("home_tab_for_you", 'news_detail_activity')) as news_click
             left join input.accounts as accounts on accounts.id = news_click.account_id where accounts.created_at > '{start_time}' and accounts.created_at < '{end_time}' and accounts.name is not null and accounts.country_code in ({self.country_code})) as account_news_click
             left join 
-            (select account_id, key, value, updated_at from partiko.memories where key like 'experiment%' and value in ({self.indicator_dimension})) as memories on memories.account_id = account_news_click.account_id where key is not null and memories.updated_at <= account_news_click.created_at 
-            
+            (select account_id, key, value, updated_at from partiko.memories where key like 'experiment%' and value in {self.indicator_dimension}) as memories on memories.account_id = account_news_click.account_id where key is not null and memories.updated_at <= account_news_click.created_at 
+
             union distinct
 
-            select account_news_impression.account_id as account_id, account_news_impression.news_id as news_id, account_news_impression.country_code as country_code, account_news_impression.placement as placement, memories.key as key, memories.value as value from 
-            (select account_id, country_code, news_impression.created_at, news_impression.placement, news_impression.news_id from
-            (select account_id, created_at, json_extract(data, '$.placement') as placement, json_extract(data, '$.id') as news_id from `stream_events.news_impression` as news_impression where news_impression.created_at > '{start_time}' and news_impression.created_at < '{end_time}' and json_value(data, '$.placement') in ("home_tab_for_you", 'news_detail_activity')) as news_impression
+            select account_news_impression.account_id as account_id, account_news_impression.country_code as country_code, account_news_impression.placement as placement, memories.key as key, memories.value as value from 
+            (select account_id, country_code, news_impression.created_at, news_impression.placement from
+            (select account_id, created_at, json_extract(data, '$.placement') as placement from `stream_events.news_impression` as news_impression where news_impression.created_at > '{start_time}' and news_impression.created_at < '{end_time}' and json_value(data, '$.placement') in ("home_tab_for_you", 'news_detail_activity')) as news_impression
             left join input.accounts as accounts on accounts.id = news_impression.account_id where accounts.created_at > '{start_time}' and accounts.created_at < '{end_time}' and accounts.name is not null and accounts.country_code in ({self.country_code})) as account_news_impression
             left join 
-            (select account_id, key, value, updated_at from partiko.memories where key like 'experiment%' and value in ({self.indicator_dimension})) as memories on memories.account_id = account_news_impression.account_id where key is not null and memories.updated_at <= account_news_impression.created_at )
-            group by news_id, country_code, placement, key, value
+            (select account_id, key, value, updated_at from partiko.memories where key like 'experiment%' and value in {self.indicator_dimension}) as memories on memories.account_id = account_news_impression.account_id where key is not null and memories.updated_at <= account_news_impression.created_at)
+            group by country_code, placement, key, value
             """
         impression_union_data = self.get_data(impression_union_sql)
 
@@ -125,6 +125,7 @@ class NewsClickCtrNewUserData(object):
             flag = True
         if flag:
             insert_sql = insert_sql[:-1]
+            print(insert_sql)
             try:
                 # 执行 sql 语句
                 cursor.execute(insert_sql)
