2252484bc2e5421593abdd1645ea49ada32b568c
Teddy Xiong <teddyxiong@buzzbreaknews.com>
2020-12-29 19:57:33 +0800

    test5

--- utils/video_watch_ctr_new_user.py
+++ utils/video_watch_ctr_new_user.py
@@ -61,53 +61,54 @@ class VideoWatchCtrNewUserData(object):
                 (select account_id, key, value, updated_at from partiko.memories where key like 'experiment%' and value in ({self.indicator_dimension})) as memories on memories.account_id = account_video_watch.account_id where key is not null and memories.updated_at <= account_video_watch.created_at) as result
                 group by country_code, placement, key, value
             """
+        print(video_watch_sql)
         video_watch_data = self.get_data(video_watch_sql)
 
-        # 新用户视频曝光数据
-        impression_sql = \
-            f"""
-                select result.country_code as country_code, result.placement as placement, result.key as key, result.value as value, count(result.account_id) as num from
-                (select distinct account_video_impression.account_id as account_id, account_video_impression.country_code as country_code, account_video_impression.placement as placement, memories.key as key, memories.value as value from 
-                (select account_id, country_code, video_impression.created_at, video_impression.placement from
-                (select account_id, created_at, json_extract_scalar(data, '$.placement') as placement from `stream_events.video_impression` as video_impression where video_impression.created_at > '{start_time}' and video_impression.created_at < '{end_time}' and json_extract_scalar(data, '$.placement') in ({self.placement})) as video_impression
-                left join input.accounts as accounts on accounts.id = video_impression.account_id where accounts.created_at > '{start_time}' and accounts.created_at < '{end_time}' and accounts.name is not null and accounts.country_code in ({self.country_code})) as account_video_impression
-                left join 
-                (select account_id, key, value, updated_at from partiko.memories where key like 'experiment%' and value in ({self.indicator_dimension})) as memories on memories.account_id = account_video_impression.account_id where key is not null and memories.updated_at <= account_video_impression.created_at) as result
-                group by country_code, placement, key, value
-            """
-        impression_data = self.get_data(impression_sql)
+        # # 新用户视频曝光数据
+        # impression_sql = \
+        #     f"""
+        #         select result.country_code as country_code, result.placement as placement, result.key as key, result.value as value, count(result.account_id) as num from
+        #         (select distinct account_video_impression.account_id as account_id, account_video_impression.country_code as country_code, account_video_impression.placement as placement, memories.key as key, memories.value as value from 
+        #         (select account_id, country_code, video_impression.created_at, video_impression.placement from
+        #         (select account_id, created_at, json_extract_scalar(data, '$.placement') as placement from `stream_events.video_impression` as video_impression where video_impression.created_at > '{start_time}' and video_impression.created_at < '{end_time}' and json_extract_scalar(data, '$.placement') in ({self.placement})) as video_impression
+        #         left join input.accounts as accounts on accounts.id = video_impression.account_id where accounts.created_at > '{start_time}' and accounts.created_at < '{end_time}' and accounts.name is not null and accounts.country_code in ({self.country_code})) as account_video_impression
+        #         left join 
+        #         (select account_id, key, value, updated_at from partiko.memories where key like 'experiment%' and value in ({self.indicator_dimension})) as memories on memories.account_id = account_video_impression.account_id where key is not null and memories.updated_at <= account_video_impression.created_at) as result
+        #         group by country_code, placement, key, value
+        #     """
+        # impression_data = self.get_data(impression_sql)
 
-        # 非曝光情况下用户观看数据
-        impression_union_sql = \
-            f"""
-                select country_code as country_code, placement as placement, key as key, value as value, count(account_id) as num from
-                (select distinct account_video_watch.account_id as account_id, account_video_watch.country_code as country_code, account_video_watch.placement as placement, memories.key as key, memories.value as value from 
-                (select account_id, country_code, video_watch.created_at, video_watch.placement from
-                (select account_id, created_at, json_extract_scalar(data, '$.placement') as placement from `stream_events.video_watch` as video_watch where video_watch.created_at > '{start_time}' and video_watch.created_at < '{end_time}' and json_extract_scalar(data, '$.placement') in ("home_tab_for_you", 'news_detail_activity')) as video_watch
-                left join input.accounts as accounts on accounts.id = video_watch.account_id where accounts.created_at > '{start_time}' and accounts.created_at < '{end_time}' and accounts.name is not null and accounts.country_code in ({self.country_code})) as account_video_watch
-                left join 
-                (select account_id, key, value, updated_at from partiko.memories where key like 'experiment%' and value in ({self.indicator_dimension})) as memories on memories.account_id = account_video_watch.account_id where key is not null and memories.updated_at <= account_video_watch.created_at 
+        # # 非曝光情况下用户观看数据
+        # impression_union_sql = \
+        #     f"""
+        #         select country_code as country_code, placement as placement, key as key, value as value, count(account_id) as num from
+        #         (select distinct account_video_watch.account_id as account_id, account_video_watch.country_code as country_code, account_video_watch.placement as placement, memories.key as key, memories.value as value from 
+        #         (select account_id, country_code, video_watch.created_at, video_watch.placement from
+        #         (select account_id, created_at, json_extract_scalar(data, '$.placement') as placement from `stream_events.video_watch` as video_watch where video_watch.created_at > '{start_time}' and video_watch.created_at < '{end_time}' and json_extract_scalar(data, '$.placement') in ("home_tab_for_you", 'news_detail_activity')) as video_watch
+        #         left join input.accounts as accounts on accounts.id = video_watch.account_id where accounts.created_at > '{start_time}' and accounts.created_at < '{end_time}' and accounts.name is not null and accounts.country_code in ({self.country_code})) as account_video_watch
+        #         left join 
+        #         (select account_id, key, value, updated_at from partiko.memories where key like 'experiment%' and value in ({self.indicator_dimension})) as memories on memories.account_id = account_video_watch.account_id where key is not null and memories.updated_at <= account_video_watch.created_at 
 
-                union distinct
+        #         union distinct
 
-                select distinct account_video_impression.account_id as account_id, account_video_impression.country_code as country_code, account_video_impression.placement as placement, memories.key as key, memories.value as value
-                from 
-                (select account_id, country_code, video_impression.created_at, video_impression.placement from
-                (select account_id, created_at, json_extract_scalar(data, '$.placement') as placement from `stream_events.video_impression` as video_impression where video_impression.created_at > '{start_time}' and video_impression.created_at < '{end_time}' and json_extract_scalar(data, '$.placement') in ("home_tab_for_you", 'news_detail_activity')) as video_impression
-                left join input.accounts as accounts on accounts.id = video_impression.account_id where accounts.created_at > '{start_time}' and accounts.created_at < '{end_time}' and accounts.name is not null and accounts.country_code in ({self.country_code})) as account_video_impression
-                left join 
-                (select account_id, key, value, updated_at from partiko.memories where key like 'experiment%' and value in ({self.indicator_dimension})) as memories on memories.account_id = account_video_impression.account_id where key is not null and memories.updated_at <= account_video_impression.created_at)
-                group by country_code, placement, key, value
-            """
-        impression_union_data = self.get_data(impression_union_sql)
+        #         select distinct account_video_impression.account_id as account_id, account_video_impression.country_code as country_code, account_video_impression.placement as placement, memories.key as key, memories.value as value
+        #         from 
+        #         (select account_id, country_code, video_impression.created_at, video_impression.placement from
+        #         (select account_id, created_at, json_extract_scalar(data, '$.placement') as placement from `stream_events.video_impression` as video_impression where video_impression.created_at > '{start_time}' and video_impression.created_at < '{end_time}' and json_extract_scalar(data, '$.placement') in ("home_tab_for_you", 'news_detail_activity')) as video_impression
+        #         left join input.accounts as accounts on accounts.id = video_impression.account_id where accounts.created_at > '{start_time}' and accounts.created_at < '{end_time}' and accounts.name is not null and accounts.country_code in ({self.country_code})) as account_video_impression
+        #         left join 
+        #         (select account_id, key, value, updated_at from partiko.memories where key like 'experiment%' and value in ({self.indicator_dimension})) as memories on memories.account_id = account_video_impression.account_id where key is not null and memories.updated_at <= account_video_impression.created_at)
+        #         group by country_code, placement, key, value
+        #     """
+        # impression_union_data = self.get_data(impression_union_sql)
 
-        # 结果存入数据库
-        cursor = mysql_client.cursor()
-        values = "country_code, placement, treatment_name, dimension, watch_num, impression_num, impression_union_num, ctr, ctr_union, start_time, end_time, create_time"
-        insert_sql = f"INSERT INTO {self.table_name} ({values}) VALUES "
-        now_time_utc = datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
-        flag = False
-        for key in impression_union_data.keys():
+        # # 结果存入数据库
+        # cursor = mysql_client.cursor()
+        # values = "country_code, placement, treatment_name, dimension, watch_num, impression_num, impression_union_num, ctr, ctr_union, start_time, end_time, create_time"
+        # insert_sql = f"INSERT INTO {self.table_name} ({values}) VALUES "
+        # now_time_utc = datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+        # flag = False
+        # for key in impression_union_data.keys():
             watch_num = video_watch_data.get(key, 0)
             impression_num = impression_data.get(key, 0)
             if impression_num <= 0:
