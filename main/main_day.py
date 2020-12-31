ecb2b6ca715a3a0001d0b525ce427b246c24f028
Teddy Xiong <teddyxiong@buzzbreaknews.com>
2020-12-31 11:59:05 +0800

    test

--- main/main_day.py
+++ main/main_day.py
@@ -21,6 +21,7 @@ from utils import video_ctr
 from utils import video_ctr_people
 from utils import new_users_retention_news_event
 from utils import new_users_retention_tab_impression
+from utils import new_users_events_retention
 from utils import old_users_events_retention
 from utils import experiment_new_users_retention_tab_impression
 from utils import partiko_memories_old_users_events_retention
@@ -48,6 +49,7 @@ KIND = {
     "video_ctr": 1,   # 视频ctr
     "video_ctr_people": 1,   # 视频 click_user_ratio
     "new_users_retention_tab_impression": 1,    # tab_impression 新用户留存
+    "new_users_events_retention": 1,    # 新用户在app_open与tab_impression下的留存
     "old_users_events_retention": 1,    # app_open与tab_impression 老用户留存
     "experiment_new_users_retention_tab_impression": 1,     # tab_impression 实验中新用户留存
     "partiko_memories_old_users_events_retention": 1,       # app_open与tab_impression 实验中老用户留存
@@ -202,6 +204,8 @@ if __name__ == "__main__":
         video_ctr_people.VideoCTRPeopleData(start_time, end_time, country_code, video_placement, indicator_dimension, "day_video_ctr_people").compute_data()
     elif kind == 'new_users_retention_tab_impression':
         new_users_retention_tab_impression.NewUsersRetentionTabImpression(start_time, end_time, 'new_users_retention_tab_impression').compute_data()
+    elif kind == 'new_users_events_retention':
+        new_users_events_retention.NewUsersEventsRetention(start_time, end_time, 'new_users_events_retention').compute_data()
     elif kind == 'old_users_events_retention':
         old_users_events_retention.OldUsersEventsRetention(start_time, end_time, 'old_users_events_retention').compute_data()
     elif kind == 'experiment_new_users_retention_tab_impression':
