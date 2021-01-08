992490c8080bf19225a690a2847d68959f54d6e5
Teddy Xiong <teddyxiong@buzzbreaknews.com>
2021-01-08 17:20:09 +0800

    test

--- upload_scripts/partiko_memories_old_users_events_retention.py
+++ upload_scripts/partiko_memories_old_users_events_retention.py
@@ -60,7 +60,7 @@ class PartikoMemoriesOldUsersEventsRetention(object):
         query = \
             f'''
             with
-            accounts as (select * from input.accounts where name is not null and created_at < '{start_time}'),
+            accounts as (select * from input.accounts where name is not null and extract(date from created_at) < extract(date from '{start_time}')),
 
             account_profiles as (select * from partiko.account_profiles where mac_address is not null),
 
