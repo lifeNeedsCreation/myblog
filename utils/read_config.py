77474e49fd1e8410b1d8e88918d6b3a4a3e11f9a
Teddy Xiong <teddyxiong@buzzbreaknews.com>
2021-01-15 20:42:45 +0800

    test

--- utils/read_config.py
+++ utils/read_config.py
@@ -3,7 +3,6 @@ from configparser import ConfigParser, ExtendedInterpolation
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-CONFIG_PATH = os.path.join(BASE_DIR, '../config/configs.ini')
-print(CONFIG_PATH)
+CONFIG_PATH = os.path.join(BASE_DIR, 'config/configs.ini')
 CONFIG = ConfigParser(interpolation=ExtendedInterpolation())
-CONFIG.read(CONFIG_PATH)
+ONFIG.read(CONFIG_PATH)
