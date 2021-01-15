1c8ae62fc0b0a93d67886b76058223134e9f3f5b
Teddy Xiong <teddyxiong@buzzbreaknews.com>
2021-01-15 20:00:07 +0800

    test

--- utils/mysql.py
+++ utils/mysql.py
@@ -20,7 +20,7 @@ class MySQL(object):
         return pymysql.connect(host=self.host, user=self.user, password=self.password, db=self.db, port=self.port)
 
 
-buzzbreak_mysql_client = MySQL(CONFIG["MYSQL_BUZZBREAK"].get(["URI"]), CONFIG["MYSQL_BUZZBREAK"].get(["PORT"]), CONFIG["MYSQL_BUZZBREAK"].get(["USER"]), CONFIG["MYSQL_BUZZBREAK"].get(["PASSWORD"]), CONFIG["MYSQL_BUZZBREAK"].get(["DATABASE"])).get_client()
-katkat_mysql_client = MySQL(CONFIG["MYSQL_KATKAT"].get(["URI"]), CONFIG["MYSQL_KATKAT"].get(["PORT"]), CONFIG["MYSQL_KATKAT"].get(["USER"]), CONFIG["MYSQL_KATKAT"].get(["PASSWORD"]), CONFIG["MYSQL_KATKAT"].get(["DATABASE"])).get_client()
+buzzbreak_mysql_client = MySQL(CONFIG["MYSQL_BUZZBREAK"].get("URI"), CONFIG["MYSQL_BUZZBREAK"].get("PORT"), CONFIG["MYSQL_BUZZBREAK"].get("USER"), CONFIG["MYSQL_BUZZBREAK"].get("PASSWORD"), CONFIG["MYSQL_BUZZBREAK"].get("DATABASE")).get_client()
+katkat_mysql_client = MySQL(CONFIG["MYSQL_KATKAT"].get("URI"), CONFIG["MYSQL_KATKAT"].get("PORT"), CONFIG["MYSQL_KATKAT"].get("USER"), CONFIG["MYSQL_KATKAT"].get("PASSWORD"), CONFIG["MYSQL_KATKAT"].get("DATABASE")).get_client()
 
 
