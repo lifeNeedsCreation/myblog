1883d3915a8c808b2b1657f2f1f19a5b03492baf
Teddy Xiong <teddyxiong@buzzbreaknews.com>
2021-01-15 20:55:11 +0800

    test

--- utils/bigquery.py
+++ utils/bigquery.py
@@ -7,22 +7,22 @@ class BigQuery(object):
     def __init__(self, project, bucket):
         self.project = project
         self.bucket = bucket
-        self.config(self.bucket)
+        self.auth = self.config(self.bucket)
+        
 
     def config(self, bucket):
         if bucket == "buzzbreak":
             AUTH_JSON_FILE_PATH = BASE_DIR + "/config/buzzbreak_bigquery_config.json"
         elif bucket == "katkat":
             AUTH_JSON_FILE_PATH = BASE_DIR + "/config/katkat_bigquery_config.json"
-        self.get_client(AUTH_JSON_FILE_PATH)
+        return AUTH_JSON_FILE_PATH
+
+    def get_client(self, auth):
+        return bigquery.Client.from_service_account_json(auth)
 
 
-    def get_client(self, auth_json_file_path):
-        return bigquery.Client.from_service_account_json(auth_json_file_path)
-
-
-buzzbreak_bigquery_client = BigQuery("buzzbreak-model-240306", "buzzbreak")
-katkat_bigquery_client = BigQuery("katkat-model-240306", "katkat")
+buzzbreak_bigquery_client = BigQuery("buzzbreak-model-240306", "buzzbreak").get_client(BigQuery("buzzbreak-model-240306", "buzzbreak").auth)
+katkat_bigquery_client = BigQuery("katkat-model-240306", "katkat").get_client(BigQuery("katkat-model-240306", "katkat").auth)
 
 
 
