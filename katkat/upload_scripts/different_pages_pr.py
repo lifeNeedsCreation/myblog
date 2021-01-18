3a43b4153b8c8221987aa9827a7dd1046e85943b
Teddy Xiong <teddyxiong@buzzbreaknews.com>
2021-01-18 14:12:25 +0800

    test

--- katkat/upload_scripts/different_pages_pr.py
+++ katkat/upload_scripts/different_pages_pr.py
@@ -60,8 +60,9 @@ class DIFFERENTPAGESPRData(object):
         inser_sql = "INSERT INTO " + self.table_name + " (placement, different_pages_num, home_pages_num, pr, start_time, end_time, create_time) VALUES"
         now_time_utc = datetime.datetime.utcnow()
         flag = False
-        print(different_pages_num)
         for key in different_pages_num.keys():
+            print("key", key)
+            print(different_pages_num)
             different_pages_num = different_pages_num.get(key, 0)
             home_pages_num = home_pages_num.get("home_tab_for_you_and_home")
             
