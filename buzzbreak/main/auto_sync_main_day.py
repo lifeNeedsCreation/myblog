a1d3b69dc9915d1e040e6d17aeaf46026608e829
Teddy Xiong <teddyxiong@buzzbreaknews.com>
2021-03-04 16:57:43 +0800

    auto sync close mysql and mongo client

--- buzzbreak/main/auto_sync_main_day.py
+++ buzzbreak/main/auto_sync_main_day.py
@@ -11,8 +11,8 @@ sys.path.append(DIR)
 
 from utils import constants
 from utils.bigquery import buzzbreak_bigquery_client
-from utils.mysql import buzzbreak_mysql_client
-from utils.mongo import buzzbreak_mongo_client
+from utils.mysql import MySQL
+from utils.mongo import Mongo
 from utils.logger import Logger
 from utils.utils import get_previous_start_time, get_today_start_time, getRestSeconds
 from utils.constants import MYSQL_INSERT_SUCCCESS, MYSQL_INSERT_FAIL
@@ -305,6 +305,8 @@ if __name__ == "__main__":
     sync_tables_str = "'" + "', '".join(sync_tables) + "'"
     fields = ["table_name", "updated_at"]
     while True:
+        buzzbreak_mysql_client = MySQL("MYSQL_BUZZBREAK")
+        buzzbreak_mongo_client = Mongo("MONGO_ANALYTICS")
         sql_mysql = "select table_name, max(updated_at) FROM {} group by table_name".format("main_day_involed_bigquery_tables")
         buzzbreak_mysql_client.execute_sql(sql_mysql)
         mysql_res = buzzbreak_mysql_client.cursor.fetchall()
@@ -352,6 +354,8 @@ if __name__ == "__main__":
             sync_end_time_str = sync_end_time.strftime(time_format)
             use_time = sync_end_time - sync_start_time
             logger.info("buzzbreak sync indicator scripts by day {} complete, use_time={}".format(sync_end_time_str, use_time))
+            buzzbreak_mysql_client.close_client()
+            buzzbreak_mongo_client.close_client()
             if use_time.total_seconds() > 60*60:
                 logger.alert("buzzbreak execute sync indicator scripts over one hour, please delay FinBI sync time")
             sleep_time = getRestSeconds(datetime.datetime.utcnow()) + 60*60*1
