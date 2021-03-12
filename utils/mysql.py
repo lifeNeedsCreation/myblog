d4214bb39569e73ffbff33a3255635e8944f08a3
Teddy Xiong <teddyxiong@buzzbreaknews.com>
2021-03-12 16:29:55 +0800

    delete excute sql info

--- utils/mysql.py
+++ utils/mysql.py
@@ -39,7 +39,7 @@ class MySQL(object):
         try:
             self.cursor.execute(sql)
             self.client.commit()
-            self.logger.info("execute sql {} success".format(sql))
+            #self.logger.info("execute sql {} success".format(sql))
         except:
             self.logger.exception("execute sql {} fail".format(sql))
             self.client.rollback()
