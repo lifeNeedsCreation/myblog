5fe11db01bc61d4ca1cd8d967fadf70efb98d64c
Teddy Xiong <teddyxiong@buzzbreaknews.com>
2021-03-23 21:33:48 +0800

    modify sql

--- katkat/SQL/posts.sql
+++ katkat/SQL/posts.sql
@@ -3,4 +3,4 @@ with
     
     posts as (select account_id, country_code, type, extract(date from created_at) as date from `partiko.posts` where country_code is not null and created_at > '{start_time}' and created_at < '{end_time}' and type is not null)
     
-    select country_code, type, date, count(*) as posts_num from posts inner join accounts on account_id = id group by country_code, type, created_date order by country_code, type, date
+    select country_code, type, date, count(*) as posts_num from posts inner join accounts on account_id = id group by country_code, type, date order by country_code, type, date
