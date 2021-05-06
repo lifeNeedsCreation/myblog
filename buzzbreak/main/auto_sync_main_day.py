97e934e25e388465dfbf182a7c8d548c9daa7a26
Teddy Xiong <teddyxiong@buzzbreaknews.com>
2021-05-06 14:54:51 +0800

    modify notice

--- buzzbreak/main/auto_sync_main_day.py
+++ buzzbreak/main/auto_sync_main_day.py
@@ -224,12 +224,12 @@ KIND = {
     "immersive_video_watch_average_rough_rank_by_model": "immersive_video_watch_average_rough_rank_by_model",      # 粗排实验平均观看次数(按模型统计)
     "immersive_video_watch_average_of_duration_rough_rank_by_model": "immersive_video_watch_average_of_duration_rough_rank_by_model",      # 粗排实验平均观看时长(按模型统计)
     "immersive_retention_rough_rank_by_model": "immersive_retention_rough_rank_by_model",   # 粗排实验留存(按模型统计)
-    "video_ctr_recall_by_model": "video_ctr_recall_by_model",     # 召回实验次数ctr(按模型统计)
-    "video_ctr_people_recall_by_model": "video_ctr_people_recall_by_model",     # 召回实验人数ctr(按模型统计)
-    "video_ctr_recall_by_bucket": "video_ctr_recall_by_bucket",     # 召回实验次数ctr(按桶统计)
-    "video_ctr_people_recall_by_bucket": "video_ctr_people_recall_by_bucket",     # 召回实验人数ctr(按桶统计)
-    "video_ctr_rough_rank_by_model": "video_ctr_rough_rank_by_model",     # 粗排实验次数ctr(按模型统计)
-    "video_ctr_people_rough_rank_by_model": "video_ctr_people_rough_rank_by_model",     # 粗排实验人数ctr(按模型统计)
+    "video_ctr_recall_by_model": "video_ctr_recall_by_model",     # 召回实验视频次数ctr(按模型统计)
+    "video_ctr_people_recall_by_model": "video_ctr_people_recall_by_model",     # 召回实验视频人数ctr(按模型统计)
+    "video_ctr_recall_by_bucket": "video_ctr_recall_by_bucket",     # 召回实验视频次数ctr(按桶统计)
+    "video_ctr_people_recall_by_bucket": "video_ctr_people_recall_by_bucket",     # 召回实验视频人数ctr(按桶统计)
+    "video_ctr_rough_rank_by_model": "video_ctr_rough_rank_by_model",     # 粗排实验视频次数ctr(按模型统计)
+    "video_ctr_people_rough_rank_by_model": "video_ctr_people_rough_rank_by_model",     # 粗排实验视频人数ctr(按模型统计)
 }
 
 class AutoSyncMainDay:
