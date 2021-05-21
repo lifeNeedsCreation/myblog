daff406be4f0d20ef980c1d644993ac87ada29d8
Teddy Xiong <teddyxiong@buzzbreaknews.com>
2021-05-21 15:15:44 +0800

    modify sql

--- buzzbreak/SQL/points_out_purpose_statistics.sql
+++ buzzbreak/SQL/points_out_purpose_statistics.sql
@@ -5,8 +5,8 @@ with
 
     accounts_point_out_info as (select country_code, p.*, json_extract_scalar(data, '$.purpose') as purpose from point_info_out as p inner join accounts on account_id = id where json_extract_scalar(data, '$.purpose') is not null),
 
-    points_out_group as (select country_code, date, purpose, sum(amount) as points_out from accounts_point_out_info group by country_code, date, purpose),
+    points_out_group as (select country_code, date, purpose, sum(amount) as points from accounts_point_out_info group by country_code, date, purpose),
 
-    points_total_out_group as (select country_code, date, sum(amount) as total_points_out from accounts_point_out_info group by country_code, date)
+    points_total_out_group as (select country_code, date, sum(amount) as total_points from accounts_point_out_info group by country_code, date)
     
-    select o.country_code as country_code, o.date as date, purpose, points_out, total_points_out, round(points_out/total_points_out, 4) as out_ratio from points_out_group as o inner join points_total_out_group as t on o.country_code = t.country_code and o.date = t.date order by country_code, date, purpose
+    select o.country_code as country_code, o.date as date, purpose, points, total_points, round(points/total_points, 4) as ratio from points_out_group as o inner join points_total_out_group as t on o.country_code = t.country_code and o.date = t.date order by country_code, date, purpose
