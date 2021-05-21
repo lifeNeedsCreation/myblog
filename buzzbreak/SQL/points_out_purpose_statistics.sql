ee00400d33fb56c22d929c99f8bf01a171b472d8
Teddy Xiong <teddyxiong@buzzbreaknews.com>
2021-05-21 15:56:27 +0800

    modify sql

--- buzzbreak/SQL/points_out_purpose_statistics.sql
+++ buzzbreak/SQL/points_out_purpose_statistics.sql
@@ -3,9 +3,9 @@ with
 
     point_info_out as (select *, extract(date from created_at) as date from partiko.point_transactions where created_at >= '{start_time}' and created_at < '{end_time}' and amount > 0 and type = 'gift'),
 
-    accounts_point_out_info as (select country_code, p.*, json_extract_scalar(data, '$.purpose') as purpose from point_info_out as p inner join accounts on account_id = id where json_extract_scalar(data, '$.purpose') is not null),
+    accounts_point_out_info as (select country_code, p.*, json_extract_scalar(data, '$.purpose') as purpose from point_info_out as p inner join accounts on account_id = id),
 
-    points_out_group as (select country_code, date, purpose, sum(amount) as points from accounts_point_out_info group by country_code, date, purpose),
+    points_out_group as (select country_code, date, (case when purpose is null then 'others' else purpose end) as purpose, sum(amount) as points from accounts_point_out_info group by country_code, date, purpose),
 
     points_total_out_group as (select country_code, date, sum(amount) as total_points from accounts_point_out_info group by country_code, date)
     
