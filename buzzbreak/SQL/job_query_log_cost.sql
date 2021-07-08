f2c27a8f143089ed85439440b394ae7fc4dcbd17
Teddy Xiong <teddyxiong@buzzbreaknews.com>
2021-07-08 14:50:54 +0800

    modify job_query_log_cost sql

--- buzzbreak/SQL/job_query_log_cost.sql
+++ buzzbreak/SQL/job_query_log_cost.sql
@@ -1,4 +1,4 @@
 with
-    a as (select extract(date from creation_time) as date, job_id, user_email, query, round(total_bytes_processed/(1024*1024*1024), 2) AS GB, round(total_bytes_processed*5/(1024*1024*1024*1000), 2) AS cost from `region-us`.INFORMATION_SCHEMA.JOBS_BY_PROJECT where creation_time >= '{start_time}' and creation_time < '{end_time}')
+    a as (select extract(date from creation_time) as date, job_id, user_email, query, ifnull(round(total_bytes_processed/(1024*1024*1024), 2), 0) AS GB, ifnull(round(total_bytes_processed*5/(1024*1024*1024*1000), 2), 0) AS cost from `region-us`.INFORMATION_SCHEMA.JOBS_BY_PROJECT where creation_time >= '{start_time}' and creation_time < '{end_time}')
 
     select date, user_email, round(sum(GB), 2) as traffic, round(sum(cost), 2) as total_cost from a group by date, user_email order by date, total_cost desc
